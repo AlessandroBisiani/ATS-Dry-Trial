2effd313c9ff64f5a3c08e200498ec34708eca93
samat24 <samat.94@gmail.com>
2014-03-31 15:09:56 +0100

    Update Model.java

--- src/support/Model.java
+++ src/support/Model.java
@@ -6,6 +6,7 @@ package gui.support;
 
 import java.sql.*;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -66,6 +67,103 @@ public class Model {
         notifyListeners();
     }
     
+    public void addBlank(String blankID, String blankStatus, String blankTypeID, String username) throws SQLException{
+        String qry = "INSERT INTO Blank VALUES ('"+blankID+"', '" + blankStatus+"', '" + blankTypeID+"', '" + username +"')";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            stmt = con.prepareStatement(qry);
+            stmt.executeUpdate();
+        }finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+        notifyListeners();
+    }
+    
+    public void addBlankType(String blankTypeID, String blankType, String numberOfCoupons) throws SQLException{
+        String qry = "INSERT INTO BlankType VALUES ('"+blankTypeID+"', '" + blankType+"', " + numberOfCoupons +")";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            stmt = con.prepareStatement(qry);
+            stmt.executeUpdate();
+        }finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+        notifyListeners();
+    }
+    
+    public void addCommission(String percentage, String commissionType) throws SQLException{
+        String qry = "INSERT INTO Commission VALUES (null, '"+percentage+"', '" + commissionType +"')";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            stmt = con.prepareStatement(qry);
+            stmt.executeUpdate();
+        }finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+        notifyListeners();
+    }
+    
+    public void addReport(String salesOfficePlace, Date periodStartDate, Date periodEndDate, String type) throws SQLException{
+        String qry = "INSERT INTO Report VALUES (null, '"+salesOfficePlace+"', '" + periodStartDate+"', '" + periodEndDate+"', '" + type +")";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            stmt = con.prepareStatement(qry);
+            stmt.executeUpdate();
+        }finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+        notifyListeners();
+    }
+    
+    public void addSale(String price, String currency, String valueInUSD, Date date, Time time, String paymentType, String customerID, String username, String blankID, int commissionID, int refunded) throws SQLException{
+        String qry = "INSERT INTO Sale VALUES (null, "+price+", '" + currency+"', " + valueInUSD+", '" + date+"', '" + time+
+        "', " + paymentType+"', " + customerID+"', " + username + "', '" + blankID + "', " + commissionID + ", " + refunded +")";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            stmt = con.prepareStatement(qry);
+            stmt.executeUpdate();
+        }finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+        notifyListeners();
+    }
+    
+    public void addSaleReport(int saleID, int reportID) throws SQLException{
+        String qry = "INSERT INTO Sale_Report VALUES (null, " + saleID + ", " + reportID +")";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            stmt = con.prepareStatement(qry);
+            stmt.executeUpdate();
+        }finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+        notifyListeners();
+    }
+    
     public void addAddress(String aline1, String aline2, String county, String city, String country, String pcode) throws SQLException{
         String qry = "INSERT INTO Address VALUES (null, '" + aline1+"', '" + aline2+"', '" + county+"', '" + city+"', '" + country+"', '" + pcode +"')";
         Connection con = null;
@@ -110,33 +208,12 @@ public class Model {
         notifyListeners();
     }
     
-    public void findAddress(String addressID) throws SQLException{
-        String qry = "SELECT * FROM Address WHERE addressID = " + addressID +")";
-        Connection con = null;
-        PreparedStatement stmt = null;
-        
-        try{
-            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
-            stmt = con.prepareStatement(qry, new String[] {"discountPlanID"});
-            stmt.executeUpdate();
-            ResultSet generatedKeys = stmt.getGeneratedKeys();
-            if (null != generatedKeys && generatedKeys.next()) {
-                lastDiscountPlanID = generatedKeys.getInt(1);
-                System.out.println(lastDiscountPlanID);
-            }
-        }finally{
-            if(con != null) try {con.close();}catch(SQLException se){}
-            if(stmt != null) try {stmt.close();}catch(SQLException se){}
-        }
-        notifyListeners();
-    }
-    
     public int getLastDiscountPlanID(){
         return lastDiscountPlanID;
     }
     
     public void addUser(String username, String password, String fname, String lname, String role) throws SQLException{
-        String qry = "INSERT INTO `User` VALUES ('" + null+"', '" + username+"', MD5('" + password+"'), '" + fname+"', '" + lname+"', '" + role +"')";
+        String qry = "INSERT INTO `User` VALUES ('"+ username+"', '" + password+"', '" + fname+"', '" + lname+"', '" + role +"')";
         Connection con = null;
         PreparedStatement stmt = null;
         
@@ -151,20 +228,98 @@ public class Model {
         notifyListeners();
     }
     
-    public void deleteCustomer(int id) throws SQLException{
-        String qry = "DELETE FROM Customer WHERE customerID='" + id + "'";
+    public void deleteCustomer(String id) throws SQLException{
+        String q = "UPDATE Sale SET customerID = null WHERE customerID = " +id;
+        String qry = "DELETE FROM Customer WHERE customerID=" + id;
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            con.prepareStatement(q).executeUpdate();
+            con.prepareStatement(qry).executeUpdate();
+        }catch(Exception e){}finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+    }
+    
+    public void deleteAddress(String id) throws SQLException{
+        String qry = "DELETE FROM Address WHERE addressID=" + id;
+        JOptionPane.showMessageDialog(null, qry);
         Connection con = null;
         PreparedStatement stmt = null;
         
         try{
             con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
             con.prepareStatement(qry).executeUpdate();
-            stmt.executeUpdate();
+        }catch(Exception e){}finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+    }
+    
+    public void deleteDiscountPlan(String id) throws SQLException{
+        String qry = "DELETE FROM DiscountPlan WHERE discountPlanID=" + id;
+        JOptionPane.showMessageDialog(null, qry);
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            con.prepareStatement(qry).executeUpdate();
         }catch(Exception e){}finally{
             if(con != null) try {con.close();}catch(SQLException se){}
             if(stmt != null) try {stmt.close();}catch(SQLException se){}
         }
         notifyListeners();
+    }
+    
+    public void updateCustomer(String id, String fname, String lname, String title, String type, int addressID, int discountPlanID) throws SQLException{
+        String qry = "UPDATE Customer SET " + " WHERE addressID=" + id;
+        JOptionPane.showMessageDialog(null, qry);
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            con.prepareStatement(qry).executeUpdate();
+        }catch(Exception e){}finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+    }
+    
+    public void updateAddress(String id, String aline1, String aline2, String county, String city, String country, String pcode) throws SQLException{
+        String qry = "UPDATE Address SET addressLine1 = '" + aline1 + "', addressLine2 = '" + aline2 + "', county = '" + county + "', city = '" + 
+                city + "', country = '" + country + "', postCode = '" + pcode + "' WHERE addressID=" + id;
+        JOptionPane.showMessageDialog(null, qry);
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            con.prepareStatement(qry).executeUpdate();
+        }catch(Exception e){}finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
+    }
+    
+    public void updateDiscountPlan(String id, String percentage, String discountType, String howAwarded, int payLaterOption) throws SQLException{
+        String qry = "UPDATE DiscountPlan SET percentage = '" + percentage + "', discountType = '" +discountType + "', howAwarded = '"
+                +percentage + "', discountType = '" + percentage + "' WHERE addressID=" + id;
+        JOptionPane.showMessageDialog(null, qry);
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/ATS?user=root&password=root");
+            con.prepareStatement(qry).executeUpdate();
+        }catch(Exception e){}finally{
+            if(con != null) try {con.close();}catch(SQLException se){}
+            if(stmt != null) try {stmt.close();}catch(SQLException se){}
+        }
     }
     
     public void addListener (ChangeListener c){
